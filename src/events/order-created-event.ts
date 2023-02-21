0605045ca191586056656af7c17c22a14c7c4f5e
Haben Tekeste <habentekeste@ZUiMac.local>
2023-02-21 07:29:36 +0400

    Updates

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -2,26 +2,24 @@ import { Subjects } from "./subjects";
 import { OrderStatus } from "./types/order-status";
 
 interface Order {
-    subject: Subjects,
-    data:{
-        id:string,
-        status:OrderStatus,
-        userId: string,
-        expiresAt: string,
-        ticket:{
-            id:string,
-            price:number
-        }
-    }
+  subject: Subjects;
+  data: {
+    id: string;
+    version: number;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
 }
 
-
-export interface OrderCreatedEvent extends Order{
-    subject: Subjects.EventOrderCreated,
-    
+export interface OrderCreatedEvent extends Order {
+  subject: Subjects.EventOrderCreated;
 }
 
-export interface OrderCreated extends Order{
-    subject: Subjects.OrderCreated,
-    
+export interface OrderCreated extends Order {
+  subject: Subjects.OrderCreated;
 }
